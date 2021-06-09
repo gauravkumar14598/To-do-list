5fa034d0e0cd0244ca6f1cd387d6a5c7febf9ddc
@gauravkumar14598 <gauravkulhari818@gmail.com>
2021-06-09 13:10:56 +0530

    added templates and layouts

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 var items = ["Buy Food", "Cook Food", "Eat Food"];
+var workItems = [];
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,13 +19,32 @@ app.get("/", function(req, res){
      month: "long"
    };
    var day = today.toLocaleDateString("en-US", options);
-    res.render("list", {KindOfDay: day, newListItems: items});
+   
+    res.render("list", {listTitle: day, newListItems: items});
   });
   app.post("/", function(req, res){
     var item = req.body.newItem;
-    items.push(item);
-   res.redirect("/");
+    if(req.body.list === "Work"){
+      workItems.push(item);
+      res.redirect("/work");
+    }
+    else{
+      items.push(item);
+      res.redirect("/");
+    }
+    
   });
+
+  app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+  });
+
+  app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+  });
+
 
 app.listen(3000, function(req, res){
     console.log("Server is up at port 3000");
